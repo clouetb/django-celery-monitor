55fb4fc599eb2440f7c15fc9b84f6fcbd9349baf
Benoît Clouet <benoit.clouet@gmail.com>
2022-01-02 23:17:13 +0100

    Fix some incompatibilities with latest version of Django

--- django_celery_monitor/admin.py
+++ django_celery_monitor/admin.py
@@ -9,7 +9,7 @@ from django.contrib.admin import helpers
 from django.contrib.admin.views import main as main_views
 from django.shortcuts import render
 from django.template import RequestContext
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.html import escape
 from django.utils.translation import gettext_lazy as _
 
@@ -17,14 +17,15 @@ from .humanize import naturaldate
 from .models import TaskState, WorkerState
 from .utils import action, display_field, fixedwidth, make_aware
 
-TASK_STATE_COLORS = {states.SUCCESS: 'green',
-                     states.FAILURE: 'red',
-                     states.REVOKED: 'magenta',
-                     states.STARTED: 'yellow',
-                     states.RETRY: 'orange',
-                     'RECEIVED': 'blue'}
-NODE_STATE_COLORS = {'ONLINE': 'green',
-                     'OFFLINE': 'gray'}
+TASK_STATE_COLORS = {
+    states.SUCCESS: "green",
+    states.FAILURE: "red",
+    states.REVOKED: "magenta",
+    states.STARTED: "yellow",
+    states.RETRY: "orange",
+    "RECEIVED": "blue",
+}
+NODE_STATE_COLORS = {"ONLINE": "green", "OFFLINE": "gray"}
 
 
 class MonitorList(main_views.ChangeList):
@@ -35,29 +36,29 @@ class MonitorList(main_views.ChangeList):
         self.title = self.model_admin.list_page_title
 
 
-@display_field(_('state'), 'state')
+@display_field(_("state"), "state")
 def colored_state(task):
     """Return the task state colored with HTML/CSS according to its level.
 
     See ``django_celery_monitor.admin.TASK_STATE_COLORS`` for the colors.
     """
     state = escape(task.state)
-    color = TASK_STATE_COLORS.get(task.state, 'black')
+    color = TASK_STATE_COLORS.get(task.state, "black")
     return '<b><span style="color: {0};">{1}</span></b>'.format(color, state)
 
 
-@display_field(_('state'), 'last_heartbeat')
+@display_field(_("state"), "last_heartbeat")
 def node_state(node):
     """Return the worker state colored with HTML/CSS according to its level.
 
     See ``django_celery_monitor.admin.NODE_STATE_COLORS`` for the colors.
     """
-    state = node.is_alive() and 'ONLINE' or 'OFFLINE'
+    state = node.is_alive() and "ONLINE" or "OFFLINE"
     color = NODE_STATE_COLORS[state]
     return '<b><span style="color: {0};">{1}</span></b>'.format(color, state)
 
 
-@display_field(_('ETA'), 'eta')
+@display_field(_("ETA"), "eta")
 def eta(task):
     """Return the task ETA as a grey "none" if none is provided."""
     if not task.eta:
@@ -65,7 +66,7 @@ def eta(task):
     return escape(make_aware(task.eta))
 
 
-@display_field(_('when'), 'tstamp')
+@display_field(_("when"), "tstamp")
 def tstamp(task):
     """Better timestamp rendering.
 
@@ -74,16 +75,18 @@ def tstamp(task):
     """
     value = make_aware(task.tstamp)
     return '<div title="{0}">{1}</div>'.format(
-        escape(str(value)), escape(naturaldate(value)),
+        escape(str(value)),
+        escape(naturaldate(value)),
     )
 
 
-@display_field(_('name'), 'name')
+@display_field(_("name"), "name")
 def name(task):
     """Return the task name and abbreviates it to maximum of 16 characters."""
     short_name = abbrtask(task.name, 16)
     return '<div title="{0}"><b>{1}</b></div>'.format(
-        escape(task.name), escape(short_name),
+        escape(task.name),
+        escape(short_name),
     )
 
 
@@ -100,9 +103,11 @@ class ModelMonitor(admin.ModelAdmin):
     def change_view(self, request, object_id, extra_context=None):
         """Make sure the title is set correctly."""
         extra_context = extra_context or {}
-        extra_context.setdefault('title', self.detail_title)
+        extra_context.setdefault("title", self.detail_title)
         return super(ModelMonitor, self).change_view(
-            request, object_id, extra_context=extra_context,
+            request,
+            object_id,
+            extra_context=extra_context,
         )
 
     def has_delete_permission(self, request, obj=None):
@@ -122,81 +127,99 @@ class ModelMonitor(admin.ModelAdmin):
 class TaskMonitor(ModelMonitor):
     """The Celery task monitor."""
 
-    detail_title = _('Task detail')
-    list_page_title = _('Tasks')
-    rate_limit_confirmation_template = (
-        'django_celery_monitor/confirm_rate_limit.html'
-    )
-    date_hierarchy = 'tstamp'
+    detail_title = _("Task detail")
+    list_page_title = _("Tasks")
+    rate_limit_confirmation_template = "django_celery_monitor/confirm_rate_limit.html"
+    date_hierarchy = "tstamp"
     fieldsets = (
-        (None, {
-            'fields': ('state', 'task_id', 'name', 'args', 'kwargs',
-                       'eta', 'runtime', 'worker', 'tstamp'),
-            'classes': ('extrapretty', ),
-        }),
-        ('Details', {
-            'classes': ('collapse', 'extrapretty'),
-            'fields': ('result', 'traceback', 'expires'),
-        }),
+        (
+            None,
+            {
+                "fields": (
+                    "state",
+                    "task_id",
+                    "name",
+                    "args",
+                    "kwargs",
+                    "eta",
+                    "runtime",
+                    "worker",
+                    "tstamp",
+                ),
+                "classes": ("extrapretty",),
+            },
+        ),
+        (
+            "Details",
+            {
+                "classes": ("collapse", "extrapretty"),
+                "fields": ("result", "traceback", "expires"),
+            },
+        ),
     )
     list_display = (
-        fixedwidth('task_id', name=_('UUID'), pt=8),
+        fixedwidth("task_id", name=_("UUID"), pt=8),
         colored_state,
         name,
-        fixedwidth('args', pretty=True),
-        fixedwidth('kwargs', pretty=True),
+        fixedwidth("args", pretty=True),
+        fixedwidth("kwargs", pretty=True),
         eta,
         tstamp,
-        'worker',
+        "worker",
     )
     readonly_fields = (
-        'state', 'task_id', 'name', 'args', 'kwargs',
-        'eta', 'runtime', 'worker', 'result', 'traceback',
-        'expires', 'tstamp',
+        "state",
+        "task_id",
+        "name",
+        "args",
+        "kwargs",
+        "eta",
+        "runtime",
+        "worker",
+        "result",
+        "traceback",
+        "expires",
+        "tstamp",
     )
-    list_filter = ('state', 'name', 'tstamp', 'eta', 'worker')
-    search_fields = ('name', 'task_id', 'args', 'kwargs', 'worker__hostname')
-    actions = ['revoke_tasks',
-               'terminate_tasks',
-               'kill_tasks',
-               'rate_limit_tasks']
+    list_filter = ("state", "name", "tstamp", "eta", "worker")
+    search_fields = ("name", "task_id", "args", "kwargs", "worker__hostname")
+    actions = ["revoke_tasks", "terminate_tasks", "kill_tasks", "rate_limit_tasks"]
 
     class Media:
         """Just some extra colors."""
 
-        css = {'all': ('django_celery_monitor/style.css', )}
+        css = {"all": ("django_celery_monitor/style.css",)}
 
-    @action(_('Revoke selected tasks'))
+    @action(_("Revoke selected tasks"))
     def revoke_tasks(self, request, queryset):
         control = Control(app=current_app)
         with current_app.default_connection() as connection:
             for state in queryset:
                 control.revoke(state.task_id, connection=connection)
 
-    @action(_('Terminate selected tasks'))
+    @action(_("Terminate selected tasks"))
     def terminate_tasks(self, request, queryset):
         control = Control(app=current_app)
         with current_app.default_connection() as connection:
             for state in queryset:
-                control.revoke(state.task_id,
-                               connection=connection,
-                               terminate=True)
+                control.revoke(state.task_id, connection=connection, terminate=True)
 
-    @action(_('Kill selected tasks'))
+    @action(_("Kill selected tasks"))
     def kill_tasks(self, request, queryset):
         control = Control(app=current_app)
         with current_app.default_connection() as connection:
             for state in queryset:
-                control.revoke(state.task_id, connection=connection,
-                               terminate=True, signal='KILL')
+                control.revoke(
+                    state.task_id, connection=connection, terminate=True, signal="KILL"
+                )
 
-    @action(_('Rate limit selected tasks'))
+    @action(_("Rate limit selected tasks"))
     def rate_limit_tasks(self, request, queryset):
         tasks = set([task.name for task in queryset])
         opts = self.model._meta
         app_label = opts.app_label
-        if request.POST.get('post'):
-            rate = request.POST['rate_limit']
+        if request.POST.get("post"):
+            rate = request.POST["rate_limit"]
             with current_app.default_connection() as connection:
                 control = Control(app=current_app)
                 for task_name in tasks:
@@ -204,27 +227,28 @@ class TaskMonitor(ModelMonitor):
             return None
 
         context = {
-            'title': _('Rate limit selection'),
-            'queryset': queryset,
-            'object_name': force_text(opts.verbose_name),
-            'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
-            'opts': opts,
-            'app_label': app_label,
+            "title": _("Rate limit selection"),
+            "queryset": queryset,
+            "object_name": force_str(opts.verbose_name),
+            "action_checkbox_name": helpers.ACTION_CHECKBOX_NAME,
+            "opts": opts,
+            "app_label": app_label,
         }
 
         return render(
-            self.rate_limit_confirmation_template, context,
+            self.rate_limit_confirmation_template,
+            context,
             context_instance=RequestContext(request),
         )
 
     def get_actions(self, request):
         actions = super(TaskMonitor, self).get_actions(request)
-        actions.pop('delete_selected', None)
+        actions.pop("delete_selected", None)
         return actions
 
     def get_queryset(self, request):
         qs = super(TaskMonitor, self).get_queryset(request)
-        return qs.select_related('worker')
+        return qs.select_related("worker")
 
 
 @admin.register(WorkerState)
@@ -232,31 +256,30 @@ class WorkerMonitor(ModelMonitor):
     """The Celery worker monitor."""
 
     can_add = True
-    detail_title = _('Node detail')
-    list_page_title = _('Worker Nodes')
-    list_display = ('hostname', node_state)
-    readonly_fields = ('last_heartbeat', )
-    actions = ['shutdown_nodes',
-               'enable_events',
-               'disable_events']
+    detail_title = _("Node detail")
+    list_page_title = _("Worker Nodes")
+    list_display = ("hostname", node_state)
+    readonly_fields = ("last_heartbeat",)
+    actions = ["shutdown_nodes", "enable_events", "disable_events"]
     control = Control(app=current_app)
 
-    @action(_('Shutdown selected worker nodes'))
+    @action(_("Shutdown selected worker nodes"))
     def shutdown_nodes(self, request, queryset):
-        self.control.broadcast('shutdown',
-                               destination=[n.hostname for n in queryset])
+        self.control.broadcast("shutdown", destination=[n.hostname for n in queryset])
 
-    @action(_('Enable event mode for selected nodes.'))
+    @action(_("Enable event mode for selected nodes."))
     def enable_events(self, request, queryset):
-        self.control.broadcast('enable_events',
-                               destination=[n.hostname for n in queryset])
+        self.control.broadcast(
+            "enable_events", destination=[n.hostname for n in queryset]
+        )
 
-    @action(_('Disable event mode for selected nodes.'))
+    @action(_("Disable event mode for selected nodes."))
     def disable_events(self, request, queryset):
-        self.control.broadcast('disable_events',
-                               destination=[n.hostname for n in queryset])
+        self.control.broadcast(
+            "disable_events", destination=[n.hostname for n in queryset]
+        )
 
     def get_actions(self, request):
         actions = super(WorkerMonitor, self).get_actions(request)
-        actions.pop('delete_selected', None)
+        actions.pop("delete_selected", None)
         return actions
